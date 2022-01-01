e933ccfe27483f9c44b85b9239415e2241809516
vaibhav saini <vbsaini2004@gmail.com>
2022-01-01 18:28:41 +0530

    added yarn

--- src/components/RoundedButton.js
+++ src/components/RoundedButton.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
 
 export const RoundedButton = ({
-  style = {},
-  textStyle = {},
-  size = 125,
-  ...props
+    style = {},
+    textStyle = {},
+    size = 125,
+    ...props
 }) => {
-  return (
-    <TouchableOpacity
-      style={[styles(size).radius, style]}
-      onPress={() => {
-        props.onPress();
-      }}>
-      <Text style={[styles(size).text, textStyle]}>{props.title}</Text>
-    </TouchableOpacity>
-  );
+    return (
+        <TouchableOpacity
+            style={[styles(size).radius, style]}
+            onPress={() => {
+                props.onPress();
+            }}
+        >
+            <Text style={[styles(size).text, textStyle]}>{props.title}</Text>
+        </TouchableOpacity>
+    );
 };
 
 const styles = (size) =>
-  StyleSheet.create({
-    radius: {
-      borderRadius: 10,
-      width: size,
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderColor: 'Black',
-      borderWidth: 2,
-    },
-    text: {
-      color: 'Black',
-      fontSize: size / 4,
-    },
-  });
+    StyleSheet.create({
+        radius: {
+            borderRadius: 10,
+            width: size,
+            alignItems: "center",
+            justifyContent: "center",
+            borderColor: "#000",
+            borderWidth: 2,
+        },
+        text: {
+            color: "#000",
+            fontSize: size / 4,
+        },
+    });
